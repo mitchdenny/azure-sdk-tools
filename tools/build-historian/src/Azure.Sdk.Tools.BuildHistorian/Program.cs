b66ee8b26631c1ecbf483122eedaa134289734ff
Mitch Denny <midenn@microsoft.com>
2019-11-02 18:39:09 -0700

    Starting point for build historian.

---
+++ tools/build-historian/src/Azure.Sdk.Tools.BuildHistorian/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Azure.Sdk.Tools.BuildHistorian
+{
+    public class Program
+    {
+        public static async Task<int> Main(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+            return 0;
+        }
+    }
+}
